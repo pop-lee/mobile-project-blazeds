e75ab68c96f908739180dd4cf84e0ff37c172de8
flex.liyunpeng@gmail.com <flex.liyunpeng@gmail.com@8f852efc-f9ae-2734-b53b-09b06b344ad0>
2013-02-02 11:54:22 +0000

    add Backyard Service

---
+++  mobile-project-blazeds/MobileBlazeDSDemo/src/com/sanrenxing/vos/BackyardUser.java
@@ -0,0 +1,35 @@
+package com.sanrenxing.vos;
+
+public class BackyardUser {
+	private int userId;
+	private String userName;
+	private String userPass;
+	private String userEmail;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPass() {
+		return userPass;
+	}
+	public void setUserPass(String userPass) {
+		this.userPass = userPass;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	
+	
+}
